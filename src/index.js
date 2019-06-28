271e18afd85e929bbce9adfa5d2398f01a22b59f
bam forrest <cameron.forr@gmail.com>
2019-06-27 23:34:53 -0500

    css changes and finished sign in pages

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
